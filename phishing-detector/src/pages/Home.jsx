f75c48f2a06acebd08d4cfd62550bac46bd4c6ee
jooh6969 <xlorddopist@gmail.com>
2025-05-29 18:58:47 +0800

    font

--- phishing-detector/src/pages/Home.jsx
+++ phishing-detector/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import SearchBar from "../components/SearchBar";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen pt-32 px-6 bg-white">
-      <h1 className="text-[6rem] font-bold font-serif  mb-6">Home Page</h1>
-      <p className="text-[3rem] font-monospace leading-relaxed mb-8">Welcome to the phishing detector.</p>
+      <h1 className="text-[4rem] font-bold font-serif  mb-6">Home Page</h1>
+      <p className="text-[2rem] font-monospace leading-relaxed mb-8">Welcome to the phishing detector.</p>
       <SearchBar />
     </div>
   );
