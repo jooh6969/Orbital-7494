a7fc3f8090b7e2c7c758bd626b84caa19bbd384d
jooh6969 <xlorddopist@gmail.com>
2025-05-29 19:08:56 +0800

    search-bar

--- phishing-detector/src/components/SearchBar.jsx
+++ phishing-detector/src/components/SearchBar.jsx
@@ -37,11 +37,11 @@ const SearchBar = () => {
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           placeholder="🔗 Enter a URL to check"
-          className="border border-gray-400 px-8 py-6 rounded text-2xl w-full shadow-md"
+          className="border border-gray-400 px-8 py-6 rounded text-[2rem] w-full shadow-md"
         />
         <button
           type="submit"
-          className="bg-blue-600 text-white px-8 py-6 rounded hover:bg-blue-700 text-2xl font-semibold shadow-md"
+          className="bg-blue-600 text-white px-8 py-6 rounded hover:bg-blue-700 text-[2rem] font-semibold shadow-md"
         >
           {loading ? "Checking..." : "Check URL"}
         </button>
